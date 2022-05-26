f4707b1684cb07986dcf11172a77b8b0d45d909b
Devavb11 <devadharshan.p2020smech@sece.ac.in>
2022-05-26 19:25:22 +0530

    deva.html

---
+++ phoenix/devadharshan.html/index.js
@@ -0,0 +1,7 @@
+function value(){
+
+    var name = document.getElementById("username").value
+    var password = document.getElementById("password").value
+     console.log([username,password])
+
+}
